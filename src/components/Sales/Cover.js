a1b07a42779cb72abd752f1a065d78eeebbce183
Beebee Ye <beebeeye@Beebees-Air.hitronhub.home>
2018-01-06 18:06:13 -0800

    change style

--- src/components/Sales/Cover.js
+++ src/components/Sales/Cover.js
@@ -4,8 +4,8 @@ export default class Cover extends React.Component {
   render() {
     return (
       <div>
-        <div>React Essentials for Designers</div>
-        <div>Master React. Implement your design the way you envision it.</div>
+        <h1>React Essentials for Designers</h1>
+        <h2>Master React. Implement your design the way you envision it.</h2>
       </div>
     );
   }
