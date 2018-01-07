a4718b56e7d0e1e7807ceb32ddff93cf5e5d6121
Beebee Ye <beebeeye@Beebees-Air.hitronhub.home>
2018-01-06 17:59:14 -0800

    add Cover Text

--- src/components/Sales/Cover.js
+++ src/components/Sales/Cover.js
@@ -3,7 +3,10 @@ import React from 'react';
 export default class Cover extends React.Component {
   render() {
     return (
-      <div>Cover</div>
+      <div>
+        <div>React Essentials for Designers</div>
+        <div>Master React. Implement your design the way you envision it.</div>
+      </div>
     );
   }
 }
