da5ff5a0288e24f2d6f671153d356a0734716049
Linton Ye <linton@jimulabs.com>
2020-12-16 20:26:47 -0800

    support images in mdx

--- nextapp/next.config.js
+++ nextapp/next.config.js
@@ -16,8 +16,24 @@ module.exports = withPlugins(
         require('remark-slug'),
         require('remark-footnotes'),
         require('remark-code-titles'),
+        require('remark-images'),
       ],
       rehypePlugins: [require('mdx-prism')],
+      webpack: (config, options) => {
+        config.module.rules.push({
+          test: /\.(svg|png|jpe?g|gif|mp4)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                publicPath: '/_next',
+                name: 'static/media/[name].[hash].[ext]',
+              },
+            },
+          ],
+        })
+        return config
+      },
     }),
   ],
   nextConfig,
