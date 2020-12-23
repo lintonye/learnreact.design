c634f48ef9d2c341aa86851995b8aebf1a6abd11
zzecila <72360420+zzecila@users.noreply.github.com>
2020-12-23 11:54:33 -0800

    initial commit

--- nextapp/src/components/Hero.tsx
+++ nextapp/src/components/Hero.tsx
@@ -1,9 +1,9 @@
-import { jsx } from '@emotion/core'
+import { jsx } from "@emotion/core";
 export function Hero() {
   return (
     <h1>
       React For Designers
-      <span className="font-cursive">and visual learners</span>
+      <span className="font-cursive text-xl">and visual learners</span>
     </h1>
-  )
+  );
 }
