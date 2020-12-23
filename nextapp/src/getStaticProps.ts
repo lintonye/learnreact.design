04b26dbc27d993fe64caec632dae2d439cf291eb
Linton Ye <linton@jimulabs.com>
2020-12-22 18:21:20 -0800

    Remove date from staticprops

--- nextapp/src/getStaticProps.ts
+++ nextapp/src/getStaticProps.ts
@@ -3,8 +3,10 @@ import getAllPostPreviews from '@/getAllPostPreviews'
 export async function getStaticProps() {
   return {
     props: {
-      // remove non-serializable field "excerpt" since it's a component
-      posts: getAllPostPreviews().map(({ excerpt, ...props }) => ({
+      // remove non-serializable fields
+      //   -- "excerpt" since it's a component
+      //   -- "date" since it's a Date
+      posts: getAllPostPreviews().map(({ excerpt, date, ...props }) => ({
         ...props,
       })),
     },
