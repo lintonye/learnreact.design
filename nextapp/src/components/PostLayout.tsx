db838c8625cfc71bb7dda170e6be00eadcdf8b3a
Linton Ye <linton@jimulabs.com>
2021-01-04 08:40:33 -0800

    Use next/image

--- nextapp/src/components/PostLayout.tsx
+++ nextapp/src/components/PostLayout.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { FunctionComponent } from 'react'
 import { NextSeo } from 'next-seo'
 import { MDXProvider } from '@mdx-js/react'
+import Image from 'next/image'
 
 type LayoutProps = {
   meta: any
@@ -20,6 +21,15 @@ const components = {
   a: (props: any) => <a className="underline" {...props} />,
   p: (props: any) => <p className="my-4 leading-relaxed" {...props} />,
   hr: (props: any) => <hr className="my-6" {...props} />,
+  img: (props: any) => (
+    <Image
+      className="mx-auto max-w-full"
+      width={500}
+      height={300}
+      layout="responsive"
+      {...props}
+    />
+  ),
   blockquote: (props: any) => (
     <blockquote
       className="pl-3 py-0.5 italic border-l-4 border-gray-300 bg-gray-100"
@@ -50,7 +60,9 @@ export const PostLayout: FunctionComponent<LayoutProps> = ({
       />
       <MDXProvider components={components}>
         <div className="prose md:prose-xl max-w-screen-md mt-0 mx-auto leading-6">
-          {title && <h1 className="text-xl leading-tight">{title}</h1>}
+          {title && (
+            <h1 className="text-3xl font-bold leading-tight">{title}</h1>
+          )}
           {children}
         </div>
       </MDXProvider>
