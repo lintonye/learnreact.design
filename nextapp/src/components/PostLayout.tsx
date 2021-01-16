9f70406990dd6d266450dfd25eb10bb66ea59eea
Linton Ye <linton@jimulabs.com>
2021-01-16 00:08:28 -0800

    Restore title font

--- nextapp/src/components/PostLayout.tsx
+++ nextapp/src/components/PostLayout.tsx
@@ -153,9 +153,9 @@ export const PostLayout: FunctionComponent<LayoutProps> = ({
               <h1
                 className={
                   'font-bold leading-tight text-center text-2xl my-10 justify-self-center ' +
-                  // 'sm:text-4xl ' +
-                  // 'md:my-16 ' +
-                  // 'lg:max-w-2xl lg:my-24 ' +
+                  'sm:text-4xl ' +
+                  'md:my-16 ' +
+                  'lg:max-w-2xl lg:my-24 ' +
                   'xl:max-w-3xl xl:text-5xl xl:my-36 '
                 }
                 css={{
