1afb15d84df07d197432027673ca2b34488be995
Linton Ye <linton@jimulabs.com>
2021-01-17 13:00:32 -0800

    Move export default to the front to fix broken mdx files

--- nextapp/next.config.js
+++ nextapp/next.config.js
@@ -109,8 +109,8 @@ module.exports = withBundleAnalyzer({
                 : [
                     'import { PostLayout } from "@/components/PostLayout"',
                     'export { getStaticProps } from "@/getStaticProps"',
-                    src,
                     'export default function PostPage(props) { return <PostLayout meta={meta} {...props} />}',
+                    src,
                   ].join('\n')
 
               if (content.includes('<!--more-->')) {
