84f2d791561159d36ee4163f0c1d74f45a54ee0c
Linton Ye <linton@jimulabs.com>
2021-01-31 10:20:43 -0800

    Connection, outline and endpoints

--- nextapp/src/components/Connection.tsx
+++ nextapp/src/components/Connection.tsx
@@ -1,4 +1,11 @@
 import { useEffect, useState } from 'react'
+import {
+  motion,
+  MotionValue,
+  transform,
+  useTransform,
+  useViewportScroll,
+} from 'framer-motion'
 
 function useViewportDimension() {
   const [dim, setDim] = useState({ width: 0, height: 0 })
@@ -14,13 +21,27 @@ function useViewportDimension() {
   return dim
 }
 
-function useViewportBoundingBox(id: string) {
+function useViewportBoundingBox(id: string): [DOMRect | undefined, number] {
   const [boundingBox, setBoundingBox] = useState<DOMRect>()
+  const [stickyTop, setStickyTop] = useState(0)
   useEffect(() => {
     const element = document.getElementById(id)
     function updateBox() {
       const box = element?.getBoundingClientRect()
-      setBoundingBox(box)
+      if (element && box) {
+        setBoundingBox(
+          DOMRectReadOnly.fromRect({
+            x: box.x,
+            y: box.y + window.scrollY,
+            width: box.width,
+            height: box.height,
+          }),
+        )
+        const style = window.getComputedStyle(element)
+        if (style.position === 'sticky') {
+          setStickyTop(element.offsetTop)
+        }
+      }
     }
     updateBox()
     if (element) {
@@ -29,7 +50,7 @@ function useViewportBoundingBox(id: string) {
       return () => observer.disconnect()
     }
   }, [id])
-  return boundingBox
+  return [boundingBox, stickyTop]
 }
 
 export function Connection({
@@ -40,22 +61,79 @@ export function Connection({
   idRight: string
 }) {
   const { width: windowWidth, height: windowHeight } = useViewportDimension()
-  const boxLeft = useViewportBoundingBox(idLeft)
-  const boxRight = useViewportBoundingBox(idRight)
-  const endpointRadius = 10
+  const [boxLeft] = useViewportBoundingBox(idLeft)
+  const [boxRight, topThreshold] = useViewportBoundingBox(idRight)
+  const endpointRadius = 6
+  const { scrollY } = useViewportScroll()
+  const yLeft = useTransform(scrollY, (sy) =>
+    boxLeft ? boxLeft.top + boxLeft.height / 2 - sy : 0,
+  )
+  const outlinePadding = 0
+  const outlineY = useTransform(scrollY, (sy) =>
+    boxLeft ? boxLeft.top - sy - outlinePadding : 0,
+  )
+  const yRight = useTransform(scrollY, (sy) => {
+    // console.log({ sy, topThreshold, top: boxRight.top + boxRight.height / 2 })
+    return boxRight
+      ? boxRight.top +
+          boxRight.height / 2 -
+          (sy > topThreshold ? topThreshold : sy)
+      : 0
+  })
+  // @ts-ignore Not sure why the line below gives type error
+  const opacity = useTransform<number, number>([yLeft, yRight], ([yl, yr]) => {
+    const input = [0, 100, windowHeight - 100, windowHeight]
+    const output = [0, 1, 1, 0]
+    return Math.min(transform(yl, input, output), transform(yr, input, output))
+  })
   return boxLeft && boxRight ? (
-    <svg
-      className="absolute inset-0"
+    <motion.svg
+      className="absolute inset-0 text-blue-400"
       viewBox={`0 0 ${windowWidth} ${windowHeight}`}
+      style={{ opacity }}
     >
-      <circle
-        cx={boxLeft.right - endpointRadius}
-        cy={boxLeft.top + boxLeft.height / 2}
+      {/* Line */}
+      <motion.line
+        x1={boxLeft.right}
+        x2={boxRight.left}
+        y1={yLeft}
+        y2={yRight}
+        stroke="currentColor"
+        strokeDasharray="4 4"
+      />
+      {/* Outline left */}
+      <motion.rect
+        x={0}
+        y={0}
+        width={boxLeft.width + outlinePadding * 2}
+        height={boxLeft.height + outlinePadding * 2}
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeDasharray="4 4"
+        rx={5}
+        fill="transparent"
+        style={{ y: outlineY, x: boxLeft.x - outlinePadding }}
+      />
+      {/* Left endpoint */}
+      <motion.circle
+        cx={boxLeft.right}
+        cy={0}
         r={endpointRadius}
         strokeWidth={2}
-        stroke="red"
-        fill="pink"
+        stroke="currentColor"
+        fill="white"
+        style={{ y: yLeft }}
       />
-    </svg>
+      {/* Right endpoint */}
+      <motion.circle
+        cx={boxRight.left}
+        cy={0}
+        r={endpointRadius}
+        strokeWidth={2}
+        stroke="currentColor"
+        fill="white"
+        style={{ y: yRight }}
+      />
+    </motion.svg>
   ) : null
 }
