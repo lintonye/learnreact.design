bf9f23399e5e646f7abc480d71ecf08ce1d9fa63
Linton Ye <linton@jimulabs.com>
2021-03-07 12:40:50 -0800

    Remove ophan pages

--- nextapp/src/pages/react.tsx
+++
@@ -1,8 +0,0 @@
-import { createTagPostList } from '@/components/PostList'
-
-const ReactPostList = createTagPostList({
-  tagSlug: 'react',
-  title: 'React',
-  description: 'All posts related to React',
-})
-export default ReactPostList
